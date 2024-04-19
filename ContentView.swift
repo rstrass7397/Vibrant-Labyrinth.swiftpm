efa60f4e22fc0471d2caeb049f47979120a1ef19
Ryan Strass <rstrass7397@stu.d214.org>
2024-04-19 12:33:46 -0500

    made the background big

--- ContentView.swift
+++ ContentView.swift
@@ -5,6 +5,7 @@ struct ContentView: View {
         HStack{
             Text("Vibrant \n Labyrinth")
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(red:255/255, green: 215/255, blue: 0/255))
     }
 }
