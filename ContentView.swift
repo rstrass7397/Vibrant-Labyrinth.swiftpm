acb2868f1888bc44b0039e36704f5c29640b21ff
Ryan Strass <rstrass7397@stu.d214.org>
2024-04-19 12:30:13 -0500

    added basic text and background color

---
+++ ContentView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            Text("Vibrant \n Labyrinth")
+        }
+        .background(Color(red:255/255, green: 215/255, blue: 0/255))
+    }
+}
