41894cd6dd991a30ae546ce3866e4a5d5dca4b14
River <dlynch@stu.d214.org>
2024-04-19 12:35:57 -0500

    i centered the title lol

--- ContentView.swift
+++ ContentView.swift
@@ -4,6 +4,7 @@ struct ContentView: View {
     var body: some View {
         HStack{
             Text("Vibrant \n Labyrinth")
+                .multilineTextAlignment(.center)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(red:255/255, green: 215/255, blue: 0/255))
