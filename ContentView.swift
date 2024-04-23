e322646edcff98dd707f6cc87c24501065fc13be
Sia Ryu <sryu5320@stu.d214.org>
2024-04-23 13:35:24 -0500

    add Navigationlink

--- ContentView.swift
+++ ContentView.swift
@@ -3,70 +3,66 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-       
-        NavigationStack{
-            VStack{
-                NavigationLink("How to play") {
-                    PlayView()
-                }
-            }
-        }
-      
-
         
-        HStack{
-            VStack(spacing: 150){
-                Text("Vibrant\nLabyrinth")
-                    .frame(width: 400, height: 200)
-                    .multilineTextAlignment(.center)
-                    .font(.system(size: 80))
-                    .foregroundColor(.black)
-                VStack(spacing: 70){
-                    Button(action: {
+        
+        NavigationLink(destination: FirstLevel()) {
+            Text("Play")
+            
+            
+            HStack{
+                VStack(spacing: 150){
+                    Text("Vibrant\nLabyrinth")
+                        .frame(width: 400, height: 200)
+                        .multilineTextAlignment(.center)
+                        .font(.system(size: 80))
+                        .foregroundColor(.black)
+                    VStack(spacing: 70){
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Play")
+                                .frame(width: 150, height: 30)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 45))
+                                .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                                    .frame(width: 150, height: 60)
+                                    .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
+                                .foregroundColor(.black)
+                                .shadow(radius: 10)
+                        })
                         
-                    }, label: {
-                        Text("Play")
-                            .frame(width: 150, height: 30)
-                            .multilineTextAlignment(.center)
-                            .font(.system(size: 45))
-                            .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                                .frame(width: 150, height: 60)
-                                .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
-                            .foregroundColor(.black)
-                            .shadow(radius: 10)
-                    })
-                    
-                    Button(action: {
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Customize")
+                                .frame(width: 260, height: 30)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 45))
+                                .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                                    .frame(width: 260, height: 60)
+                                    .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
+                                .foregroundColor(.black)
+                                .shadow(radius: 10)
+                        })
                         
-                    }, label: {
-                        Text("Customize")
-                            .frame(width: 260, height: 30)
-                            .multilineTextAlignment(.center)
-                            .font(.system(size: 45))
-                            .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                                .frame(width: 260, height: 60)
-                                .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
-                            .foregroundColor(.black)
-                            .shadow(radius: 10)
-                    })
-                    
-                    
-                    NavigationLink(destination: PlayView()) {
-                        Text("How To Play")
-                            .frame(width: 290, height: 30)
-                            .multilineTextAlignment(.center)
-                            .font(.system(size: 45))
-                            .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                                .frame(width: 290, height: 60)
-                                .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
-                            .foregroundColor(.black)
-                            .shadow(radius: 10)
+                        NavigationLink(destination: PlayView()) {
+                            Text("How To Play")
+                                .frame(width: 290, height: 30)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 45))
+                                .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                                    .frame(width: 290, height: 60)
+                                    .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
+                                .foregroundColor(.black)
+                                .shadow(radius: 10)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(red:255/255, green: 215/255, blue: 80/255))
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color(red:255/255, green: 215/255, blue: 80/255))
+            
         }
-        
     }
 }
+
---
+++ FirstLevel.swift
@@ -0,0 +1,16 @@
+//
+//  FirstLevel.swift
+//  Vibrant Labyrinth
+//
+//  Created by Sia Ryu on 4/23/24.
+//
+
+import SwiftUI
+
+struct FirstLevel:View {
+    var body: some View {
+        Text("set up the first level here")
+            .navigationTitle("Level 1")
+    }
+}
+
