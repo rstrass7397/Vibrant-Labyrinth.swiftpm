342086710d8be023452957eea058b3eb9485e337
Ryan Strass <rstrass7397@stu.d214.org>
2024-04-23 13:35:12 -0500

    figured out continous movement when holding button

--- ControllerView.swift
+++ ControllerView.swift
@@ -8,31 +8,70 @@
 import SwiftUI
 
 struct ControllerView: View {
+    @State var timer: Timer?
+    @State var xPos: CGFloat = 200
+    @State var yPos: CGFloat = 300
     var body: some View {
+        Rectangle()
+            .frame(width: 100, height: 100)
+            .position(x: xPos,y: yPos)
         VStack {
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            Button(action:{
+                yPos -= 10
+            }, label: {
                 Image(systemName: "arrowshape.up.fill")
                     .font(.largeTitle)
             })
+            
+            
             HStack {
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Button(action: {
+                    xPos -= 10
+                }, label: {
                     Image(systemName: "arrowshape.left.fill")
                         .font(.largeTitle)
+                        .onTapGesture {
+                            self.leftAction()
+                        }
+                        .gesture(
+                            LongPressGesture()
+                                .onEnded { _ in
+                                    print("start...")
+                                    self.timer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { _ in
+                                        self.leftAction()
+                                    }
+                                }
+                                .sequenced(before:
+                                            DragGesture(minimumDistance: 0)
+                                    .onEnded { _ in
+                                        print("end...")
+                                        self.timer?.invalidate()
+                                        self.timer = nil
+                                    }
+                                          )
+                        )
                 })
                 
-                    Image(systemName: "pencil.tip")
-                        .font(.title)
-                        .opacity(0)
+                Image(systemName: "pencil.tip")
+                    .font(.title)
+                    .opacity(0)
                 
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Button(action: {
+                    xPos += 10
+                }, label: {
                     Image(systemName: "arrowshape.right.fill")
                         .font(.largeTitle)
                 })
             }
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            Button(action: {
+                yPos += 10
+            }, label: {
                 Image(systemName: "arrowshape.down.fill")
                     .font(.largeTitle)
             })
         }
     }
-}
+        func leftAction() {
+            xPos -= 10
+        }
+    }
