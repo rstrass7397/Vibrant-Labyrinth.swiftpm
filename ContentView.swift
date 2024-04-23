56180c79912a990852b19dd415b4eaf315cbd8ec
Sia Ryu <sryu5320@stu.d214.org>
2024-04-23 12:37:26 -0500

    Add the navigation link and send to the next view, but need to adject the button's design and font.

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack{
-            Text("Vibrant \n Labyrinth")
-                .multilineTextAlignment(.center)
+        NavigationView {
+            VStack{
+                NavigationLink(destination: PlayView()) {
+                    Text("Instruction")
+                }
+                
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(red:255/255, green: 215/255, blue: 0/255))
     }
 }
---
+++ PlayView.swift
@@ -0,0 +1,15 @@
+//
+//  PlayView.swift
+//  Vibrant Labyrinth
+//
+//  Created by Sia Ryu on 4/23/24.
+//
+
+import SwiftUI
+
+struct PlayView: View {
+    var body: some View {
+        Text("put the instrction here")
+        .navigationTitle("How to play")
+    }
+}
