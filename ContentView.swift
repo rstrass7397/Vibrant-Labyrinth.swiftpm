951e018d676b1c0a59de78022d69add3bb611b37
Ryan Strass <rstrass7397@stu.d214.org>
2024-04-29 13:22:30 -0500

    making progress on level container view

--- ContentView.swift
+++ ContentView.swift
@@ -3,60 +3,60 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-    
+        
         NavigationView{
-        HStack{
-            VStack(spacing: 150){
-                Text("Vibrant\nLabyrinth")
-                    .frame(width: 400, height: 200)
-                    .multilineTextAlignment(.center)
-                    .font(.system(size: 80))
-                    .foregroundColor(.black)
-                    .shadow(radius: 5)
-                VStack(spacing: 70){
-                    Button(action: {
+            HStack{
+                VStack(spacing: 150){
+                    Text("Vibrant\nLabyrinth")
+                        .frame(width: 400, height: 200)
+                        .multilineTextAlignment(.center)
+                        .font(.system(size: 80))
+                        .foregroundColor(.black)
+                        .shadow(radius: 5)
+                    VStack(spacing: 70){
                         
-                    }, label: {
-                        Text("Play")
-                            .frame(width: 150, height: 30)
-                            .multilineTextAlignment(.center)
-                            .font(.system(size: 45))
-                            .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                                .frame(width: 150, height: 60)
-                                .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
-                            .foregroundColor(.black)
-                            .shadow(radius: 5)
-                    })
-                    
-                    Button(action: {
+                        NavigationLink(destination: LevelContainerView()
+                        ) {
+                            Text("Play")
+                                .frame(width: 150, height: 30)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 45))
+                                .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                                    .frame(width: 150, height: 60)
+                                    .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
+                                .foregroundColor(.black)
+                                .shadow(radius: 5)
+                        }
                         
-                    }, label: {
-                        Text("Customize")
-                            .frame(width: 260, height: 30)
-                            .multilineTextAlignment(.center)
-                            .font(.system(size: 45))
-                            .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                                .frame(width: 260, height: 60)
-                                .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
-                            .foregroundColor(.black)
-                            .shadow(radius: 5)
-                    })
-                    
-                    
-                    NavigationLink(destination: PlayView()) {
-                        Text("How To Play")
-                            .frame(width: 290, height: 30)
-                            .multilineTextAlignment(.center)
-                            .font(.system(size: 45))
-                            .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                                .frame(width: 290, height: 60)
-                                .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
-                            .foregroundColor(.black)
-                            .shadow(radius: 5)
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Customize")
+                                .frame(width: 260, height: 30)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 45))
+                                .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                                    .frame(width: 260, height: 60)
+                                    .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
+                                .foregroundColor(.black)
+                                .shadow(radius: 5)
+                        })
+                        
+                        
+                        NavigationLink(destination: PlayView()) {
+                            Text("How To Play")
+                                .frame(width: 290, height: 30)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 45))
+                                .background(RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                                    .frame(width: 290, height: 60)
+                                    .foregroundColor(Color(red: 252/255, green: 176/255, blue: 1/255)))
+                                .foregroundColor(.black)
+                                .shadow(radius: 5)
+                        }
                     }
                 }
             }
-        }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color(red:255/255, green: 215/255, blue: 80/255))
         }
