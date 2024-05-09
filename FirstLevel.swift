919d60a5a50742ed9e6e5ebcb46975856ae1caa1
Ryan Strass <rstrass7397@stu.d214.org>
2024-05-09 13:38:34 -0500

    updated

--- FirstLevel.swift
+++ FirstLevel.swift
@@ -1,62 +1,19 @@
-//
-//  FirstLevel.swift
-//  Vibrant Labyrinth
-//
-//  Created by Sia Ryu on 4/23/24.
-//
 
+import Foundation
 import SwiftUI
-//@State var xPos1: CGFloat = 200
- //  @State var yPos1: CGFloat = 300
-   
-  // @State var xPos2: CGFloat = 200
-  // @State var yPos2: CGFloat = 600
-   
-  // @State var collision: Bool = false
+
 struct FirstLevel:View {
     var body: some View {
-        Text("set up the first level here")
-            .navigationTitle("Level 1")
         
-//    }
-//
-//           ZStack{
-//             Circle()
-//                .fill(Color.black)
-//                .frame(width: 100, height:100)
-//                .positon(x:self.xPos1, y:self.yPos1)
-//
-//
-//                           Circle()
-//                            .fill(Color.blue)
-//                                .frame(width: 100, height: 100)
-//                                .position(x: self.xPos2, y: self.yPos2)
-//                                .gesture(
-//                                    DragGesture()
-//                                        .onChanged({ value in
-//                                            self.xPos2 = value.location.x
-//                                            self.yPos2 = value.location.y
-//                                            self.checkCollision()
-//                                        })
-//                                )
-//                            self.collision ? Text("Collision! :(") : nil
-//
-//
-//                        }
-//
-//                    }
-//
-//                    func checkCollision (){
-//
-//                        if abs(self.xPos1 - self.xPos2) <  100 && abs(self.yPos1 - self.yPos2) < 100 {
-//                            self.collision = true
-//                        } else {
-//                            self.collision = false
-//                        }
-                    }
-                 
-        //This is a placeholder. Please make this view (and every other level view) 400 x 400
-    
+        VStack{
+            Rectangle()
+                .frame(width: 400, height: 400)
+                .foregroundColor(.black)
+            ControllerView()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.white)
     }
+}
 
 
